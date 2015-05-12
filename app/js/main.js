5ac99a7778f87de6c4000bc0c6149d14f3f49262
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-05-12 12:29:05 +0100

    Preloader added

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 
+function preLoad(){
+    var loader = $(document.getElementById('main-loader')),
+        content = $(document.getElementById('main-content'));
+
+    setTimeout(function(){
+        loader.fadeOut();
+        content.removeClass('hidden');
+    }, 4000);
+}
+
 
 $(document).ready(function(){
     $('.slider').slider({full_width: true});
+
+    preLoad();
 });
